a537288a8cbd29cdb55741189f722222142c4271
Ryan <ryanmichaelgarber@gmail.com>
2025-11-30 18:44:18 -0500

    Set output to server

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   site: "https://canvastest.pvcccodingclub.workers.dev.com",
+  output: "server",
   integrations: [mdx(), sitemap()],
   adapter: cloudflare({
     platformProxy: {
